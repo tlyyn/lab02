d1acc7ace116200af548ec7511d67101960a9094
tlyyn <lin.tarass@gmail.com>
2022-10-12 22:36:34 +0300

    first commit

---
+++ Project2/Source.cpp
@@ -0,0 +1,28 @@
+﻿// Lab_02.cpp
+// Лин Тарас
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 13
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double a; // вхідний параметр
+	double b; // вхідний параметр
+	double z1; // резултьтат обчислення 1-го виразу
+	//double z2; // результат обчислення 2-го виразу
+
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+
+	z1 = (sin(a) + cos(2 * b - a)) / (cos(a) - sin(2 * b - a));
+	//z2 = (1 + sin(2 * b)) / (cos(2 * b));
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
